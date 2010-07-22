8e0b384aa0ed0e741b341c9071b100b227f1a762
unknown <smorlat@.(none)>
2010-07-22 17:17:54 +0200

    still does not work

--- src/nowebcam.c
+++ src/nowebcam.c
@@ -17,9 +17,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#ifdef WIN32
-#define UNICODE
-#endif
 
 #ifdef HAVE_CONFIG_H
 #include "mediastreamer-config.h"
