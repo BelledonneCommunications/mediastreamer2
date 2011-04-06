9c3a750b794e161fcea2dd1e36f98f0c8b90eb88
Guillaume Beraudo <guillaume.beraudo@belledonne-communications.com>
2011-04-06 09:35:00 +0200

    Don't use translations in Android.

--- build/android/Android.mk
+++ build/android/Android.mk
@@ -141,6 +141,7 @@ LOCAL_SRC_FILES += gsm.c
 LOCAL_CFLAGS += \
 	-UHAVE_CONFIG_H \
 	-include $(LOCAL_PATH)/../build/android/libmediastreamer2_AndroidConfig.h \
+	-DN_\(String\)=\(String\) \
 	-D_POSIX_SOURCE -Wall
 
 
