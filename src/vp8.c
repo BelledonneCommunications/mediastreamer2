4add780c3789f470a05974fa2a448b36d2e81475
Simon Morlat <simon.morlat@linphone.org>
2011-09-20 11:17:01 +0200

    improve vpx8 detection

--- src/vp8.c
+++ src/vp8.c
@@ -1,5 +1,5 @@
  /*
- * vp8.c -Android Media plugin for Linphone-
+ * vp8.c -VP8 encoder/decoder wrapper
  *
  *
  * Copyright (C) 2011  Belledonne Communications, Grenoble, France
@@ -12,7 +12,7 @@
  *  This program is distributed in the hope that it will be useful,
  *  but WITHOUT ANY WARRANTY; without even the implied warranty of
  *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU Library General Public License for more details.
+ *  GNU General Public License for more details.
  *
  *  You should have received a copy of the GNU General Public License
  *  along with this program; if not, write to the Free Software
