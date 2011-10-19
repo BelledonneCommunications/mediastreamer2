c422124f6fc3cc090ab591627577c94cb9eb2e05
Jehan Monnier <jehan.monnier@linphone.org>
2011-10-19 15:51:52 +0200

    fix compilation issue

--- src/aqsnd.c
+++ src/aqsnd.c
@@ -57,7 +57,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #endif
 
 #include <AudioToolbox/AudioToolbox.h>
-#ifndef __ios
+#if !TARGET_OS_IPHONE
 #include <CoreAudio/AudioHardware.h>
 #endif
 
