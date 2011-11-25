c18e8a3e87cd1e972829c6a23efea44ecc4a5612
Guillaume Beraudo <guillaume.beraudo@belledonne-communications.com>
2011-11-25 12:04:15 +0100

    API to test presence of built in echo canceller in Android.

--- java/src/org/linphone/mediastream/Version.java
+++ java/src/org/linphone/mediastream/Version.java
@@ -35,7 +35,11 @@ public class Version {
 	public static final int API07_ECLAIR_21 = 7;
 	public static final int API08_FROYO_22 = 8;
 	public static final int API09_GINGERBREAD_23 = 9;
+	public static final int API10_GINGERBREAD_MR1_233 = 10;
 	public static final int API11_HONEYCOMB_30 = 11;
+	public static final int API12_HONEYCOMB_MR1_31X = 12;
+	public static final int API13_HONEYCOMB_MR2_32  = 13;
+	public static final int API14_ICE_CREAM_SANDWICH_40 = 14;
 
 	private static native boolean nativeHasZrtp();
 	private static native boolean nativeHasNeon();
@@ -43,6 +47,7 @@ public class Version {
 
 	private static final int buildVersion = 
 		Integer.parseInt(Build.VERSION.SDK);
+//		API03_CUPCAKE_15;
 //		8; // 2.2
 //		7; // 2.1
 
