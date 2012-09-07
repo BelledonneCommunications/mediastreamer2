e269a75b492a8b3188bc49b87c337a814c310e1e
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2012-09-07 12:26:06 +0200

    Add shell script to synchronize oRTP dependencies.

---
+++ src/ortp-deps/update_ortp.sh
@@ -0,0 +1,17 @@
+#!/bin/sh
+
+BASEDIR=$(dirname $0)
+GITURL=git://git.linphone.org/ortp.git
+TEMPGITDIR=ortp-temp
+
+cd $BASEDIR
+rm -rf $TEMPGITDIR
+git clone $GITURL $TEMPGITDIR
+
+mkdir -p ortp
+cp -f $TEMPGITDIR/include/ortp/port.h ortp/
+cp -f $TEMPGITDIR/include/ortp/str_utils.h ortp/
+cp -f $TEMPGITDIR/src/port.c .
+cp -f $TEMPGITDIR/src/str_utils.c .
+
+rm -rf $TEMPGITDIR
