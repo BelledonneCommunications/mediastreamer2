0be82819d8195676b53d176be4c63353058a4d7b
Guillaume Beraudo <guillaume.beraudo@linphone.org>
2012-10-09 12:45:04 +0200

    Fix compilation of msv4l.c on non linux.
    Fix Debian issue Bug#688182: kFreeBSD support

--- src/msv4l.c
+++ src/msv4l.c
@@ -17,8 +17,6 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
-#ifdef __linux
-
 #include "mediastreamer-config.h"
 
 #include <fcntl.h>
@@ -1072,4 +1070,3 @@ static void v4l_detect(MSWebCamManager *obj){
 }
 
 
-#endif
