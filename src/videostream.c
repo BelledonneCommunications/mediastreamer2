95ef77fe123c23a71e3fe1cb8932efaf048f399c
Jehan Monnier <jehan.monnier@linphone.org>
2012-10-11 15:48:43 +0200

    remove size converter in case of IOS simulator

--- src/videostream.c
+++ src/videostream.c
@@ -333,7 +333,7 @@ static void configure_video_source(VideoStream *stream){
 		vsize=cam_vsize;
 		ms_message("Output video size adjusted to match camera resolution (%ix%i)\n",vsize.width,vsize.height);
 	} else if (cam_vsize.width*cam_vsize.height>vsize.width*vsize.height){
-#ifdef __arm__
+#if TARGET_IPHONE_SIMULATOR || defined(__arm__)
 		ms_error("Camera is proposing a size bigger than encoder's suggested size (%ix%i > %ix%i) "
 		           "Using the camera size as fallback because cropping or resizing is not implemented for arm.",
 		           cam_vsize.width,cam_vsize.height,vsize.width,vsize.height);
