145e9b981b3554d4dd8a969163a97d61545f0809
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2013-03-05 14:54:06 +0100

    Add the voidsource filter for Windows.

--- build/vsx/mediastreamer2/mediastreamer2/basedescs.h
+++ build/vsx/mediastreamer2/mediastreamer2/basedescs.h
@@ -2,6 +2,7 @@
 
 extern MSFilterDesc ms_tee_desc;
 extern MSFilterDesc ms_join_desc;
+extern MSFilterDesc ms_void_source_desc;
 extern MSFilterDesc ms_void_sink_desc;
 extern MSFilterDesc ms_itc_source_desc;
 extern MSFilterDesc ms_itc_sink_desc;
@@ -9,6 +10,7 @@ extern MSFilterDesc ms_itc_sink_desc;
 MSFilterDesc * ms_base_filter_descs[]={
 &ms_tee_desc,
 &ms_join_desc,
+&ms_void_source_desc,
 &ms_void_sink_desc,
 &ms_itc_source_desc,
 NULL
