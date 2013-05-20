09751b86e9761c4550d741979e60c54c9f70ec8b
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2013-05-20 14:16:11 +0200

    Use a VideoStream for the video tester.

--- build/vsx/mediastreamer2-tester-wp8/mediastreamer2-tester-native/mediastreamer2-tester-video.cpp
+++ build/vsx/mediastreamer2-tester-wp8/mediastreamer2-tester-native/mediastreamer2-tester-video.cpp
@@ -22,25 +22,12 @@ namespace mediastreamer2_tester_native
 		void stop();
 
 	private:
-		void createTicker();
-		void createFilters();
-		void createGraphs();
-		void destroyGraphs();
-		void destroyFilters();
-		void destroyTicker();
-
-		MSTicker *m_ticker;
-		MSFilter *m_video_capture;
-		MSFilter *m_video_display;
-		MSFilter *m_rtp_send;
-		MSFilter *m_rtp_recv;
-		RtpSession *m_rtps;
+		VideoStream *m_video_stream;
 	};
 }
 
 Mediastreamer2TesterVideoPrivate::Mediastreamer2TesterVideoPrivate()
-	: m_ticker(nullptr), m_video_capture(nullptr), m_video_display(nullptr),
-	m_rtp_send(nullptr), m_rtp_recv(nullptr), m_rtps(nullptr)
+	: m_video_stream(nullptr)
 {
 }
 
@@ -49,30 +36,6 @@ Mediastreamer2TesterVideoPrivate::~Mediastreamer2TesterVideoPrivate()
 }
 
 void Mediastreamer2TesterVideoPrivate::start()
-{
-	ms_filter_reset_statistics();
-	createTicker();
-	createFilters();
-	createGraphs();
-}
-
-void Mediastreamer2TesterVideoPrivate::stop()
-{
-	destroyGraphs();
-	ms_filter_log_statistics();
-	destroyFilters();
-	destroyTicker();
-}
-
-void Mediastreamer2TesterVideoPrivate::createTicker()
-{
-	MSTickerParams params;
-	params.name = "Video Tester MSTicker";
-	params.prio = MS_TICKER_PRIO_NORMAL;
-	m_ticker = ms_ticker_new_with_params(&params);
-}
-
-void Mediastreamer2TesterVideoPrivate::createFilters()
 {
 	MSWebCamManager *cam_manager;
 	MSWebCam *camera;
@@ -85,83 +48,18 @@ void Mediastreamer2TesterVideoPrivate::createFilters()
 	if (camera == NULL) {
 		camera = ms_web_cam_manager_get_default_cam(cam_manager);
 	}
-	m_video_capture = ms_web_cam_create_reader(camera);
-	m_video_display = ms_filter_new_from_name("MSWP8Dis");
-	m_rtp_send = ms_filter_new(MS_RTP_SEND_ID);
-	m_rtp_recv = ms_filter_new(MS_RTP_RECV_ID);
-	m_rtps = create_duplex_rtpsession(20000, 0, FALSE);
-	rtp_session_set_remote_addr_full(m_rtps, "127.0.0.1", 20000, NULL, 0);
-	rtp_session_set_payload_type(m_rtps, 102);
-	rtp_session_enable_rtcp(m_rtps,FALSE);
-	ms_filter_call_method(m_rtp_send, MS_RTP_SEND_SET_SESSION, m_rtps);
-	ms_filter_call_method(m_rtp_recv, MS_RTP_RECV_SET_SESSION, m_rtps);
+
+	ms_filter_reset_statistics();
+	m_video_stream = video_stream_new(20000, 0, FALSE);
+	video_stream_set_display_filter_name(m_video_stream, "MSWP8Dis");
+	video_stream_start(m_video_stream, &av_profile, "127.0.0.1", 20000, NULL, 0, 102, 0, camera);
 }
 
-void Mediastreamer2TesterVideoPrivate::createGraphs()
+void Mediastreamer2TesterVideoPrivate::stop()
 {
-	MSConnectionHelper h;
-
-	// Sending graph
-	ms_connection_helper_start(&h);
-	ms_connection_helper_link(&h, m_video_capture, -1, 0);
-	ms_connection_helper_link(&h, m_rtp_send, 0, -1);
-
-	// Receiving graph
-	ms_connection_helper_start(&h);
-	ms_connection_helper_link(&h, m_rtp_recv, -1, 0);
-	ms_connection_helper_link(&h, m_video_display, 0, -1);
-
-	ms_ticker_attach_multiple(m_ticker, m_video_capture, m_rtp_recv, NULL);
-}
-
-void Mediastreamer2TesterVideoPrivate::destroyGraphs()
-{
-	MSConnectionHelper h;
-
-	ms_ticker_detach(m_ticker, m_video_capture);
-	ms_ticker_detach(m_ticker, m_rtp_recv);
-
-	// Sending graph
-	ms_connection_helper_start(&h);
-	ms_connection_helper_unlink(&h, m_video_capture, -1, 0);
-	ms_connection_helper_unlink(&h, m_rtp_send, 0, -1);
-
-	// Receiving graph
-	ms_connection_helper_start(&h);
-	ms_connection_helper_unlink(&h, m_rtp_recv, -1, -0);
-	ms_connection_helper_unlink(&h, m_video_display, 0, -1);
-}
-
-void Mediastreamer2TesterVideoPrivate::destroyFilters()
-{
-	if (m_rtps != nullptr) {
-		rtp_session_destroy(m_rtps);
-		m_rtps = nullptr;
-	}
-	if (m_rtp_recv != nullptr) {
-		ms_filter_destroy(m_rtp_recv);
-		m_rtp_recv = nullptr;
-	}
-	if (m_rtp_send != nullptr) {
-		ms_filter_destroy(m_rtp_send);
-		m_rtp_send = nullptr;
-	}
-	if (m_video_display != nullptr) {
-		ms_filter_destroy(m_video_display);
-		m_video_display = nullptr;
-	}
-	if (m_video_capture != nullptr) {
-		ms_filter_destroy(m_video_capture);
-		m_video_capture = nullptr;
-	}
-}
-
-void Mediastreamer2TesterVideoPrivate::destroyTicker()
-{
-	if (m_ticker != nullptr) {
-		ms_ticker_destroy(m_ticker);
-		m_ticker = nullptr;
-	}
+	ms_filter_log_statistics();
+	video_stream_stop(m_video_stream);
+	m_video_stream = NULL;
 }
 
 Mediastreamer2TesterVideo::Mediastreamer2TesterVideo()
