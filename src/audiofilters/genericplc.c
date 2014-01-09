cee9401413e0ffe876401d8d9b9abd4f72e34420
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2014-01-09 16:19:10 +0100

    Fix compilation of genericplc on Windows with Visual Studio.

--- src/audiofilters/genericplc.c
+++ src/audiofilters/genericplc.c
@@ -17,6 +17,7 @@
  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  */
 
+#include <stdint.h>
 #include <mediastreamer2/msfilter.h>
 #include <mediastreamer2/mscodecutils.h>
 #include <mediastreamer2/msticker.h>
