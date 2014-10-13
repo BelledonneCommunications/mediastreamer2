8cdefcf5bed896bc75c632acf5dc4e3ef7d312d6
François Grisez <francois.grisez@grisezf.fr>
2014-10-13 13:36:09 +0200

    Fix crach while opening MKV file

--- src/videofilters/mkv.c
+++ src/videofilters/mkv.c
@@ -1,3 +1,22 @@
+/*
+mediastreamer2 library - modular sound and video processing and streaming
+Copyright (C) 2006  Simon MORLAT (simon.morlat@linphone.org)
+
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+*/
+
 #define bool_t ms_bool_t
 #include "mediastreamer2/msfilter.h"
 #include "mediastreamer2/msvideo.h"
@@ -873,7 +892,7 @@ static ms_bool_t matroska_load_file(Matroska *obj) {
 			if(obj->nbClusters == 0) {
 				obj->firstCluster = obj->cluster;
 			}
-			MATROSKA_LinkClusterBlocks((matroska_cluster *)obj->cluster, obj->segment, obj->tracks, FALSE);
+			MATROSKA_LinkClusterBlocks((matroska_cluster *)obj->cluster, obj->info, obj->tracks, FALSE);
 			obj->nbClusters++;
 		}
 	}
