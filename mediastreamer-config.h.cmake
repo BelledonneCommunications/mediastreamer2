4e06a438c0f5416d62f92112828b2f1d0844f324
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2015-01-13 16:13:15 +0100

    Add definition of HAVE_POLARSSL_SSL_H when building with CMake if polarssl has been found.

--- mediastreamer-config.h.cmake
+++ mediastreamer-config.h.cmake
@@ -76,5 +76,6 @@
 #cmakedefine HAVE_GL 1
 #cmakedefine HAVE_X11_XLIB_H 1
 #cmakedefine HAVE_XV 1
-#cmakedefine HAVE_LINUX_VIDEODEV_H
-#cmakedefine HAVE_LINUX_VIDEODEV2_H
+#cmakedefine HAVE_LINUX_VIDEODEV_H 1
+#cmakedefine HAVE_LINUX_VIDEODEV2_H 1
+#cmakedefine HAVE_POLARSSL_SSL_H 1
