9d600a34ebf0a7c7a94cac36898f4fbca0dcd09a
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2015-02-24 23:00:23 +0100

    Actually qosanalyzer is needed, adding it back with correct location

--- tester/mediastreamer2_adaptive_tester.c
+++ tester/mediastreamer2_adaptive_tester.c
@@ -25,6 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "mediastreamer2/mstonedetector.h"
 #include "mediastreamer2_tester.h"
 #include "mediastreamer2_tester_private.h"
+#include "../src/voip/qosanalyzer.h"
 
 #include <stdio.h>
 #include "CUnit/Basic.h"
