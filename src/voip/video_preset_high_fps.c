4b3211ea7aa00acf1bc4f6f46180fa4d59d100a0
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2015-06-11 09:40:25 +0200

    Add custom video preset.

--- src/voip/video_preset_high_fps.c
+++ src/voip/video_preset_high_fps.c
@@ -24,37 +24,42 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include "mediastreamer2/msvideopresets.h"
 
 
+static MSVideoConfiguration custom_conf_list[] = {
+	MS_VIDEO_CONF(0, 10000000, HDTVP, 30, 1)
+};
+
 
 static MSVideoConfiguration high_fps_desktop_vp8_conf_list[] = {
-	MS_VIDEO_CONF(1536000,  2560000, SXGA_MINUS, 25, 4),
-	MS_VIDEO_CONF(800000,  2000000,       720P, 25, 4),
-	MS_VIDEO_CONF(800000,  1536000,        XGA, 25, 4),
-	MS_VIDEO_CONF( 600000,  1024000,       SVGA, 25, 2),
-	MS_VIDEO_CONF( 350000,   600000,        VGA, 25, 2),
-	MS_VIDEO_CONF( 350000,   600000,        VGA, 15, 1),
-	MS_VIDEO_CONF( 200000,   350000,        CIF, 18, 1),
-	MS_VIDEO_CONF( 150000,   200000,       QVGA, 15, 1),
-	MS_VIDEO_CONF( 100000,   150000,       QVGA, 10, 1),
-	MS_VIDEO_CONF(  64000,   100000,       QCIF, 12, 1),
-	MS_VIDEO_CONF(      0,    64000,       QCIF,  5 ,1)
+	MS_VIDEO_CONF(1536000,  2560000,  720P, 30, 4),
+	MS_VIDEO_CONF( 800000,  2000000,   XGA, 30, 4),
+	MS_VIDEO_CONF( 800000,  1536000,  SVGA, 30, 4),
+	MS_VIDEO_CONF( 600000,  1024000,   VGA, 30, 2),
+	MS_VIDEO_CONF( 350000,   600000,   CIF, 30, 2),
+	MS_VIDEO_CONF( 350000,   600000,   CIF, 30, 1),
+	MS_VIDEO_CONF( 200000,   350000,  QVGA, 30, 1),
+	MS_VIDEO_CONF( 150000,   200000,  QCIF, 30, 1),
+	MS_VIDEO_CONF( 100000,   150000,  QCIF, 20, 1),
+	MS_VIDEO_CONF(  64000,   100000,  QCIF, 12, 1),
+	MS_VIDEO_CONF(      0,    64000,  QCIF,  5 ,1)
 };
 
 static MSVideoConfiguration high_fps_embedded_vp8_conf_list[] = {
-	MS_VIDEO_CONF(2048000, 2560000,       UXGA, 12, 2),
-	MS_VIDEO_CONF(1024000, 1536000, SXGA_MINUS, 12, 2),
-	MS_VIDEO_CONF( 750000, 1024000,        XGA, 12, 2),
-	MS_VIDEO_CONF( 500000,  750000,       SVGA, 12, 2),
-	MS_VIDEO_CONF( 300000,  500000,        VGA, 12, 2),
-	MS_VIDEO_CONF( 100000,  300000,       QVGA, 18, 2),
-	MS_VIDEO_CONF(  64000,  100000,       QCIF, 12, 2),
-	MS_VIDEO_CONF(300000, 600000,          VGA, 12, 1),
-	MS_VIDEO_CONF(100000, 300000,         QVGA, 10, 1),
-	MS_VIDEO_CONF( 64000, 100000,         QCIF, 10, 1),
+	MS_VIDEO_CONF(2048000, 2560000,       720P, 30, 2),
+	MS_VIDEO_CONF( 750000, 1536000,       SVGA, 30, 2),
+	MS_VIDEO_CONF( 500000,  750000,        VGA, 30, 2),
+	MS_VIDEO_CONF( 300000,  500000,       QVGA, 30, 2),
+	MS_VIDEO_CONF( 150000,  300000,       QVGA, 18, 2),
+	MS_VIDEO_CONF(  64000,  100000,       QCIF, 15, 2),
+	MS_VIDEO_CONF( 300000,  600000,       QVGA, 30, 1),
+	MS_VIDEO_CONF( 150000,  200000,       QCIF, 30, 1),
+	MS_VIDEO_CONF( 100000,  150000,       QCIF, 20, 1),
+	MS_VIDEO_CONF(  64000,  100000,       QCIF, 10, 1),
 	MS_VIDEO_CONF(      0,   64000,       QCIF,  5, 1)
 };
 
 
 void register_video_preset_high_fps(MSVideoPresetsManager *manager) {
+	ms_video_presets_manager_register_preset_configuration(manager, "custom", NULL, custom_conf_list);
 	ms_video_presets_manager_register_preset_configuration(manager, "high-fps", "desktop,vp8", high_fps_desktop_vp8_conf_list);
 	ms_video_presets_manager_register_preset_configuration(manager, "high-fps", "embedded,vp8", high_fps_embedded_vp8_conf_list);
 }
