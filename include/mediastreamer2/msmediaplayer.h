219420f6f0243fcd1cc855fbc9f05a9601f374d5
François Grisez <francois.grisez@belledonne-communications.com>
2015-09-23 11:58:25 +0200

    Remove @breaf key words from the documentation of msmediaplayer.h

--- include/mediastreamer2/msmediaplayer.h
+++ include/mediastreamer2/msmediaplayer.h
@@ -25,7 +25,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <mediastreamer2/msvideo.h>
 
 /**
- * @brief Media file player
+ * Media file player
  */
 typedef struct _MSMediaPlayer MSMediaPlayer;
 
@@ -45,7 +45,7 @@ extern "C"{
 #endif
 
 /**
- * @brief Instanciate a media player
+ * Instanciate a media player
  * @param snd_card Playback sound card
  * @param video_display_name Video out
  * @param window_id Pointer on the drawing window
@@ -54,20 +54,20 @@ extern "C"{
 MS2_PUBLIC MSMediaPlayer *ms_media_player_new(MSSndCard *snd_card, const char *video_display_name, void *window_id);
 
 /**
- * @brief Free a media player
+ * Free a media player
  * @param obj Pointer on the MSMediaPlayer to free
  */
 MS2_PUBLIC void ms_media_player_free(MSMediaPlayer *obj);
 
 /**
- * @brief Get the window ID
+ * Get the window ID
  * @param obj The player
  * @return The window ID
  */
 MS2_PUBLIC void * ms_media_player_get_window_id(const MSMediaPlayer *obj);
 
 /**
- * @brief Set the "End of File" callback
+ * Set the "End of File" callback
  * @param obj The player
  * @param cb Function to call
  * @param user_data Data which will be passed to the function
@@ -75,7 +75,7 @@ MS2_PUBLIC void * ms_media_player_get_window_id(const MSMediaPlayer *obj);
 MS2_PUBLIC void ms_media_player_set_eof_callback(MSMediaPlayer *obj, MSMediaPlayerEofCallback cb, void *user_data);
 
 /**
- * @brief Open a media file
+ * Open a media file
  * @param obj The player
  * @param filepath Path of the file to open
  * @return TRUE if the file could be opened
@@ -83,21 +83,21 @@ MS2_PUBLIC void ms_media_player_set_eof_callback(MSMediaPlayer *obj, MSMediaPlay
 MS2_PUBLIC bool_t ms_media_player_open(MSMediaPlayer *obj, const char *filepath);
 
 /**
- * @brief Close a media file
+ * Close a media file
  * That function can be safly call even if no file has been opend
  * @param obj The player
  */
 MS2_PUBLIC void ms_media_player_close(MSMediaPlayer *obj);
 
 /**
- * @brief Start playback
+ * Start playback
  * @param obj The player
  * @return TRUE if playback has been successfuly started
  */
 MS2_PUBLIC bool_t ms_media_player_start(MSMediaPlayer *obj);
 
 /**
- * @brief Stop a playback
+ * Stop a playback
  * When a playback is stoped, the player automatically seek at
  * the begining of the file.
  * @param obj The player
@@ -105,13 +105,13 @@ MS2_PUBLIC bool_t ms_media_player_start(MSMediaPlayer *obj);
 MS2_PUBLIC void ms_media_player_stop(MSMediaPlayer *obj);
 
 /**
- * @brief Turn playback to paused.
+ * Turn playback to paused.
  * @param obj The player
  */
 MS2_PUBLIC void ms_media_player_pause(MSMediaPlayer *obj);
 
 /**
- * @brief Seek into the opened file
+ * Seek into the opened file
  * Can be safly call when playback is runing
  * @param obj The player
  * @param seek_pos_ms Position where to seek on (in milliseconds)
@@ -120,34 +120,34 @@ MS2_PUBLIC void ms_media_player_pause(MSMediaPlayer *obj);
 MS2_PUBLIC bool_t ms_media_player_seek(MSMediaPlayer *obj, int seek_pos_ms);
 
 /**
- * @brief Get the state of the player
+ * Get the state of the player
  * @param obj The player
  * @return An MSPLayerSate enum
  */
 MS2_PUBLIC MSPlayerState ms_media_player_get_state(MSMediaPlayer *obj);
 
 /**
- * @brief Get the duration of the opened media
+ * Get the duration of the opened media
  * @param obj The player
  * @return The duration in milliseconds. -1 if failure
  */
 MS2_PUBLIC int ms_media_player_get_duration(MSMediaPlayer *obj);
 
 /**
- * @brief Get the position of the playback
+ * Get the position of the playback
  * @param obj The player
  * @return The position in milliseconds. -1 if failure
  */
 MS2_PUBLIC int ms_media_player_get_current_position(MSMediaPlayer *obj);
 
 /**
- * @brief Check whether Matroska format is supported by the player
+ * Check whether Matroska format is supported by the player
  * @return TRUE if supported
  */
 MS2_PUBLIC bool_t ms_media_player_matroska_supported(void);
 
 /**
- * @brief Return format of the current opened file
+ * Return format of the current opened file
  * @param obj Player
  * @return Format of the file. UNKNOWN_FORMAT when no file is opened
  */
