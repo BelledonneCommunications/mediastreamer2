04513bf862ac0c85f392f1353a168b31957a6ffa
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2016-01-26 15:14:31 +0100

    Use correct namespaces and winmd name when building for Windows 10.

--- tester/mediastreamer2_tester_windows.h
+++ tester/mediastreamer2_tester_windows.h
@@ -3,52 +3,58 @@
 #include "mediastreamer2/mscommon.h"
 #include "mediastreamer2_tester.h"
 
-namespace mediastreamer2_tester_runtime
+namespace BelledonneCommunications
 {
-	public interface class OutputTraceListener
+	namespace Mediastreamer2
 	{
-	public:
-		void outputTrace(Platform::String^ lev, Platform::String^ msg);
-	};
-
-    public ref class MS2Tester sealed
-    {
-    public:
-		void setOutputTraceListener(OutputTraceListener^ traceListener);
-		unsigned int nbTestSuites();
-		unsigned int nbTests(Platform::String^ suiteName);
-		Platform::String^ testSuiteName(int index);
-		Platform::String^ testName(Platform::String^ suiteName, int testIndex);
-		void initialize(Windows::Storage::StorageFolder^ writableDirectory, Platform::Boolean ui);
-		bool run(Platform::String^ suiteName, Platform::String^ caseName, Platform::Boolean verbose);
-		void runAllToXml();
-		void initVideo();
-		void uninitVideo();
-		void startVideoStream(Platform::Object^ CaptureElement, Platform::Object^ MediaElement, Platform::String^ camera, Platform::String^ codec, Platform::String^ videoSize, unsigned int frameRate, unsigned int bitRate);
-		void stopVideoStream();
-		int getOrientation() { return _deviceRotation; }
-		void setOrientation(int degrees);
-		void changeCamera(Platform::String^ camera);
-
-		static property MS2Tester^ Instance
+		namespace Tester
 		{
-			MS2Tester^ get() { return _instance; }
-		}
-		property Windows::Foundation::IAsyncAction^ AsyncAction
-		{
-			Windows::Foundation::IAsyncAction^ get() { return _asyncAction; }
-		}
-		property Windows::Foundation::Collections::IVector<Platform::String^>^ VideoDevices
-		{
-			Windows::Foundation::Collections::IVector<Platform::String^>^ get();
-		}
-	private:
-		MS2Tester();
-		~MS2Tester();
+			public interface class OutputTraceListener
+			{
+			public:
+				void outputTrace(Platform::String^ lev, Platform::String^ msg);
+			};
 
-		static MS2Tester^ _instance;
-		Windows::Foundation::IAsyncAction^ _asyncAction;
-		VideoStream *_videoStream;
-		int _deviceRotation;
-	};
+			public ref class NativeTester sealed
+			{
+			public:
+				void setOutputTraceListener(OutputTraceListener^ traceListener);
+				unsigned int nbTestSuites();
+				unsigned int nbTests(Platform::String^ suiteName);
+				Platform::String^ testSuiteName(int index);
+				Platform::String^ testName(Platform::String^ suiteName, int testIndex);
+				void initialize(Windows::Storage::StorageFolder^ writableDirectory, Platform::Boolean ui);
+				bool run(Platform::String^ suiteName, Platform::String^ caseName, Platform::Boolean verbose);
+				void runAllToXml();
+				void initVideo();
+				void uninitVideo();
+				void startVideoStream(Platform::Object^ CaptureElement, Platform::Object^ MediaElement, Platform::String^ camera, Platform::String^ codec, Platform::String^ videoSize, unsigned int frameRate, unsigned int bitRate);
+				void stopVideoStream();
+				int getOrientation() { return _deviceRotation; }
+				void setOrientation(int degrees);
+				void changeCamera(Platform::String^ camera);
+
+				static property NativeTester^ Instance
+				{
+					NativeTester^ get() { return _instance; }
+				}
+				property Windows::Foundation::IAsyncAction^ AsyncAction
+				{
+					Windows::Foundation::IAsyncAction^ get() { return _asyncAction; }
+				}
+				property Windows::Foundation::Collections::IVector<Platform::String^>^ VideoDevices
+				{
+					Windows::Foundation::Collections::IVector<Platform::String^>^ get();
+				}
+			private:
+				NativeTester();
+				~NativeTester();
+
+				static NativeTester^ _instance;
+				Windows::Foundation::IAsyncAction^ _asyncAction;
+				VideoStream *_videoStream;
+				int _deviceRotation;
+			};
+		}
+	}
 }
