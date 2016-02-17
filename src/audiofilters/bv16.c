dbfb05e1f0e3bbae3fc62caec290eedc8daa7cbe
Sandrine Avakian <sandrine.avakian@belledonne-communications.com>
2016-02-17 17:58:36 +0100

    Adding dec_process.

--- src/audiofilters/bv16.c
+++ src/audiofilters/bv16.c
@@ -18,6 +18,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 */
 
 #include "mediastreamer2/msfilter.h"
+#include "mediastreamer2/msticker.h"
+
 #include <bv16-floatingpoint/bvcommon/typedef.h>
 #include <bv16-floatingpoint/bv16/bv16cnst.h>
 #include <bv16-floatingpoint/bvcommon/bvcommon.h>
@@ -30,6 +32,8 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 #include <malloc.h>
 #define alloca _alloca
 #endif
+
+#define NO_OF_BYTES_PER_5MS     10
 
 typedef struct EncState{
 	struct BV16_Encoder_State state;
@@ -206,8 +210,23 @@ MSFilterDesc ms_bv16_enc_desc={
 
 #endif
 
+typedef struct DecState{
+	struct BV16_Decoder_State state;
+	uint32_t ts; // timestamp
+	int sample_time;
+	int maxptime;
+	int nsamples;
+	int frame_size;
+	int size_samples;
+	MSBufferizer *bufferizer;
+} DecState;
+
 static void dec_init(MSFilter *f){
-	Reset_BV16_Decoder((struct BV16_Decoder_State *)f->data);
+	DecState *s=ms_new0(DecState,1);
+	Reset_BV16_Decoder(&s->state);
+	f->data = s;
+	s->sample_time = 0;
+	
 }
 
 static void dec_uninit(MSFilter *f){
@@ -217,25 +236,74 @@ static void dec_uninit(MSFilter *f){
 
 
 static void dec_process(MSFilter *f){
-	// gsm s=(gsm)f->data;
-	// mblk_t *im;
-	// mblk_t *om;
-	// const int frsz=160*2;
 
-	// while((im=ms_queue_get(f->inputs[0]))!=NULL){
-	// 	for (;(im->b_wptr-im->b_rptr)>=33;im->b_rptr+=33) {
-	// 		om=allocb(frsz,0);
-	// 		mblk_meta_copy(im, om);
-	// 		if (gsm_decode(s,(gsm_byte*)im->b_rptr,(gsm_signal*)om->b_wptr)<0){
-	// 			ms_warning("gsm_decode error!");
-	// 			freemsg(om);
-	// 		}else{
-	// 			om->b_wptr+=frsz;
-	// 			ms_queue_put(f->outputs[0],om);
-	// 		}
-	// 	}
-	// 	freemsg(im);
-	// }
+	DecState *s=(DecState*)f->data;
+
+
+	mblk_t *im, *om;
+	//int nbytes;
+	int frsz;
+	struct	BV16_Bit_Stream bs;
+	int rem_bytes;
+	frsz = ((sizeof(short)) * FRSZ ) / 8 ; //size in bytes
+	int frame_per_packet = 0  ;
+	while((im=ms_queue_get(f->inputs[0]))!=NULL){
+		do{
+			rem_bytes = ((im->b_wptr-im->b_rptr));
+			om=allocb(frsz ,0);			
+			mblk_meta_copy(im,om);
+			if ( rem_bytes  < NO_OF_BYTES_PER_5MS){
+				ms_warning("bv16 packet loss ? ");
+				BV16_PLC(&s->state,(short*)om->b_wptr);
+				mblk_set_plc_flag(om, 1);
+			}
+			
+			else{
+				BV16_BitUnPack(im->b_rptr+frame_per_packet*frsz, &bs);
+				BV16_Decode(&bs, &s->state, (short*)om->b_wptr);  
+				im->b_rptr += NO_OF_BYTES_PER_5MS; 
+				
+				frame_per_packet ++;
+			}
+			if (s->sample_time==0) s->sample_time=f->ticker->time;
+			s->sample_time+=5; 
+			om->b_wptr += frsz ;
+			
+			ms_queue_put(f->outputs[0],om);
+		
+		
+		}while(rem_bytes >= NO_OF_BYTES_PER_5MS);
+	}
+	freemsg(im);
+		// nbytes=msgdsize(im);
+		// if (nbytes==0  || nbytes%NO_OF_BYTES_PER_5MS!=0 ){
+		// 	freemsg(im);
+		// 	continue;
+		// }
+		// if (nbytes%NO_OF_BYTES_PER_5MS ==0){
+		// 	frame_per_packet = nbytes/NO_OF_BYTES_PER_5MS;
+		
+		// 	for (k=0;k<frame_per_packet;k++) {
+		// 		om=allocb(frsz ,0);
+		// 		BV16_BitUnPack(im->b_rptr+k*frsz, &bs);
+		// 		BV16_Decode(&bs, &s->state, (short*)om->b_wptr);   
+		// 	//	WebRtcIlbcfix_Decode(s->ilbc_dec, , s->nbytes, (int16_t *)om->b_wptr, &speech_type);
+		// 		om->b_wptr += frsz * 8;
+		// 		mblk_meta_copy(im,om);
+		// 		ms_queue_put(f->outputs[0],om);
+		// 	}
+		// 	if 
+			
+		// }
+		// freemsg(im);
+	//}
+	
+}
+
+static int dec_have_plc(MSFilter *f, void *arg)
+{
+	*((int *)arg) = 1;
+	return 0;
 }
 
 static int dec_get_sample_rate(MSFilter *f, void *arg) {
@@ -246,6 +314,7 @@ static int dec_get_sample_rate(MSFilter *f, void *arg) {
 
 static MSFilterMethod dec_methods[] = {
 	{ MS_FILTER_GET_SAMPLE_RATE, dec_get_sample_rate },
+	{ 	MS_DECODER_HAVE_PLC		, 	dec_have_plc	},
 	{ 0,                         NULL                }
 };
 
