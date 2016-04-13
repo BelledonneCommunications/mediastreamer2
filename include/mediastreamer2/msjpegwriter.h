6921d8748f9f1002f7da24922569a5e2b120376d
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2016-04-13 19:32:21 +0200

    Added jpeg_writer filter in video preview graph

--- include/mediastreamer2/msjpegwriter.h
+++ include/mediastreamer2/msjpegwriter.h
@@ -22,6 +22,7 @@ Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
 
 #include <mediastreamer2/msfilter.h>
 
-#define MS_JPEG_WRITER_TAKE_SNAPSHOT	MS_FILTER_METHOD(MS_JPEG_WRITER_ID,0,const char)
+#define MS_JPEG_WRITER_TAKE_SNAPSHOT	MS_FILTER_METHOD(MS_JPEG_WRITER_ID, 0, const char)
+#define MS_JPEG_WRITER_SNAPSHOT_TAKEN 	MS_FILTER_EVENT(MS_JPEG_WRITER_ID, 0, const char)
 
 #endif
