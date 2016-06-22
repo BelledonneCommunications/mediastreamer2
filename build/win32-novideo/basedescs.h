b108e13f8d8239e6d12c19dcbe96efb3b1d7f20c
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2016-06-22 18:38:01 +0200

    Remove obsolete and no longer maintained Visual Studio projects.

--- build/win32-novideo/basedescs.h
+++
@@ -1,9 +0,0 @@
-#include "mediastreamer2/msfilter.h"
-
-extern MSFilterDesc ms_tee_desc;
-extern MSFilterDesc ms_join_desc;
-MSFilterDesc * ms_base_filter_descs[]={
-&ms_tee_desc,
-&ms_join_desc,
-NULL
-};
