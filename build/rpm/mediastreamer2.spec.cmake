4b6f895c96f56b0bfc3d242479cc151863aa9acc
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2018-02-05 17:40:15 +0100

    Initialize CMAKE_BUILD_TYPE in RPM spec file.

--- build/rpm/mediastreamer2.spec.cmake
+++ build/rpm/mediastreamer2.spec.cmake
@@ -61,7 +61,7 @@ develop programs using the mediastreamer2 library.
 %setup -n %{name}-%{version}%{?build_number_ext}
 
 %build
-%{expand:%%%cmake_name} . -DCMAKE_INSTALL_LIBDIR=%{_lib} -DCMAKE_PREFIX_PATH:PATH=%{_prefix} @RPM_ALL_CMAKE_OPTIONS@
+%{expand:%%%cmake_name} . -DCMAKE_BUILD_TYPE=@CMAKE_BUILD_TYPE@ -DCMAKE_INSTALL_LIBDIR=%{_lib} -DCMAKE_PREFIX_PATH:PATH=%{_prefix} @RPM_ALL_CMAKE_OPTIONS@
 make %{?_smp_mflags}
 
 %install
