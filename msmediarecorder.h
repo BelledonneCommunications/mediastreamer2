a57fd17404a0c42cebf09e783a9205d0acb528ca
Benjamin Verdier <benjamin.verdier@belledonne-communications.com>
2018-07-13 11:55:13 +0200

    Add recorder

---
+++ msmediarecorder.h
@@ -0,0 +1,12 @@
+//
+//  msmediarecorder.h
+//  mediastreamer2
+//
+//  Created by benjamin_verdier on 11/07/2018.
+//
+
+#ifndef msmediarecorder_h
+#define msmediarecorder_h
+
+
+#endif /* msmediarecorder_h */
