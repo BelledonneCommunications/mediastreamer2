e3b2bcd5b9408201b0d073e84add63d866d3f072
Simon Morlat <simon.morlat@belledonne-communications.com>
2019-08-14 15:30:02 +0200

    Change license to GPLv2 to GPLv3 for all files having Gplv2 license.

--- src/ortp-deps/utils.h
+++ src/ortp-deps/utils.h
@@ -1,10 +1,21 @@
-/***************************************************************************
- *            utils.h
+/*
+ * Copyright (c) 2010-2019 Belledonne Communications SARL.
  *
- *  Wed Feb 23 14:15:36 2005
- *  Copyright  2005  Simon Morlat
- *  Email simon.morlat@linphone.org
- ****************************************************************************/
+ * This file is part of mediastreamer2.
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
 /*
   The oRTP library is an RTP (Realtime Transport Protocol - rfc3550) stack.
   Copyright (C) 2001  Simon MORLAT simon.morlat@linphone.org
--- tools/filters_graph.py
+++ tools/filters_graph.py
@@ -1,12 +1,13 @@
 #!/usr/bin/python
 #
-# mediastreamer2 library - modular sound and video processing and streaming
-# Copyright (C) 2006-2013 Belledonne Communications, Grenoble
+# Copyright (c) 2010-2019 Belledonne Communications SARL.
 #
-# This program is free software; you can redistribute it and/or
-# modify it under the terms of the GNU General Public License
-# as published by the Free Software Foundation; either version 2
-# of the License, or (at your option) any later version.
+# This file is part of mediastreamer2.
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
 #
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
@@ -14,8 +15,8 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU General Public License
-# along with this program; if not, write to the Free Software
-# Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+#
 
 import argparse
 import os
