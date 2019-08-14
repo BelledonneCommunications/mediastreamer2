8647fd6cc4cf92ac231bd655fd879bbd2fc69b2d
Simon Morlat <simon.morlat@belledonne-communications.com>
2019-08-14 15:30:02 +0200

    Change license to GPLv2 to GPLv3 for all files having Gplv2 license.

--- src/videofilters/vp8.c
+++ src/videofilters/vp8.c
@@ -1,22 +1,20 @@
- /*
- * vp8.c -VP8 encoder/decoder wrapper
+/*
+ * Copyright (c) 2010-2019 Belledonne Communications SARL.
  *
+ * This file is part of mediastreamer2.
  *
- * Copyright (C) 2011  Belledonne Communications, Grenoble, France
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
- *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "mediastreamer2/msfilter.h"
