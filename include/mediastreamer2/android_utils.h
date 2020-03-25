03ca2425c2d3c38bdc23df152c6e03c9ffce55e5
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2020-03-25 13:53:43 +0100

    Update ms2 base files and streams as well as OpenSLES to support new audio routes

--- include/mediastreamer2/android_utils.h
+++ include/mediastreamer2/android_utils.h
@@ -20,62 +20,60 @@
 #ifndef android_utils_h
 #define android_utils_h
 
+#include <mediastreamer2/mssndcard.h>
 #include <mediastreamer2/msfilter.h>
 #include <jni.h>
 
-enum _DeviceType{
-	TELEPHONY,
-	AUX_LINE,
-	GENERIC_USB,
-	HEADSET,
-	MICROPHONE,
-	EARPIECE,
-	HEADPHONES,
-	SPEAKER,
-	BLUETOOTH,
-	UNKNOWN
-};
-
 /**
- * Android device type enum.
+ * Retrieve preferred buffer size from Mediastreamer Android Context.
 **/
-typedef enum _DeviceType DeviceType;
+MS2_PUBLIC int get_preferred_buffer_size();
 
 /**
  * Retrieve preferred sample rate from Mediastreamer Android Context.
 **/
-int get_preferred_sample_rate();
+MS2_PUBLIC int get_preferred_sample_rate();
 
 /**
  * Retrieve all devices in a given direction.
  * Valid values for string dir are "all" "output" "input"
 **/
-jobject get_all_devices(JNIEnv *env, const char * dir);
+MS2_PUBLIC jobjectArray get_all_devices(JNIEnv *env, const char * dir);
 
 /**
  * Retrieve device ID for the device deviceInfo.
 **/
-unsigned int get_device_id(JNIEnv *env, jobject deviceInfo);
+MS2_PUBLIC unsigned int get_device_id(JNIEnv *env, jobject deviceInfo);
 
 /**
  * Retrieve the value of a JV class field fieldName.
 **/
-int getJVIntField(JNIEnv *env, const char * className, const char * fieldName);
+MS2_PUBLIC int getJVIntField(JNIEnv *env, const char * className, const char * fieldName);
 
 /**
  * Retrieve device type for the device deviceInfo.
 **/
-DeviceType get_device_type(JNIEnv *env, jobject deviceInfo);
+MS2_PUBLIC MSSndCardDeviceType get_device_type(JNIEnv *env, jobject deviceInfo);
 
 /**
  * Retrieve device capabilities (recorder or playback) for the device deviceInfo.
 **/
-unsigned int get_device_capabilities(JNIEnv *env, jobject deviceInfo);
+MS2_PUBLIC unsigned int get_device_capabilities(JNIEnv *env, jobject deviceInfo);
 
 /**
  * Retrieve product name for the device deviceInfo.
 **/
-char * get_device_product_name(JNIEnv *env, jobject deviceInfo);
+MS2_PUBLIC char * get_device_product_name(JNIEnv *env, jobject deviceInfo);
+
+/**
+ * Retrieve SDK version the app is running on.
+**/
+MS2_PUBLIC int get_sdk_version(JNIEnv *env);
+
+/**
+ * Make upcalls to change device from mediastreamer.
+**/
+MS2_PUBLIC void change_device(JNIEnv *env, MSSndCardDeviceType type);
 
 #ifdef __cplusplus
 extern "C"{
--- include/mediastreamer2/msinterfaces.h
+++ include/mediastreamer2/msinterfaces.h
@@ -296,6 +296,12 @@ typedef enum _MSRecorderState MSRecorderState;
 	MS_FILTER_METHOD(MSFilterAudioCaptureInterface, 0, float)
 #define MS_AUDIO_CAPTURE_GET_VOLUME_GAIN \
 	MS_FILTER_METHOD(MSFilterAudioCaptureInterface, 1, float)
+/** Set capture device ID */
+#define MS_AUDIO_CAPTURE_SET_INTERNAL_ID \
+	MS_FILTER_METHOD(MSFilterAudioCaptureInterface, 2, int)
+/** Get capture device ID */
+#define MS_AUDIO_CAPTURE_GET_INTERNAL_ID \
+	MS_FILTER_METHOD(MSFilterAudioCaptureInterface, 3, int)
 #define MS_AUDIO_CAPTURE_FORCE_SPEAKER_STATE \
 	MS_FILTER_METHOD(MSFilterAudioCaptureInterface, 255, bool_t)
 
@@ -314,6 +320,12 @@ typedef enum _MSAudioRoute MSAudioRoute;
 	MS_FILTER_METHOD(MSFilterAudioPlaybackInterface, 2, MSAudioRoute)
 #define MS_AUDIO_PLAYBACK_MUTE \
 	MS_FILTER_METHOD(MSFilterAudioPlaybackInterface, 3, int)
+/** Set playback device ID */
+#define MS_AUDIO_PLAYBACK_SET_INTERNAL_ID \
+	MS_FILTER_METHOD(MSFilterAudioPlaybackInterface, 4, int)
+/** Get playback device ID */
+#define MS_AUDIO_PLAYBACK_GET_INTERNAL_ID \
+	MS_FILTER_METHOD(MSFilterAudioPlaybackInterface, 5, int)
 
 /** Interface definitions for audio encoder */
 #define MS_AUDIO_ENCODER_SET_PTIME \
