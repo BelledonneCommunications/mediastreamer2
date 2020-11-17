15716d2bd831cb36b7eb393343a0fb889882288e
François Grisez <francois.grisez@belledonne-communications.com>
2020-11-17 14:57:38 +0100

    MKVPlayer: allow to seek in MKV file without CUEs table
    The whole MKVReader code has been migrated into C++

--- src/utils/mkv_reader.h
+++ src/utils/mkv_reader.h
@@ -17,31 +17,58 @@
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
-#ifndef _MKV_PARSER_H
-#define _MKV_PARSER_H
+#pragma once
+
+#include <memory>
+#include <list>
+#include <vector>
 
 #include "mediastreamer2/mscommon.h"
 
-#define MAX_MKV_STRING_LENGTH 256
+class MKVReader;
 
-typedef struct _MKVReader MKVReader;
+template <typename T>
+struct NodeDeleter {
+	void operator()(T *ptr) {NodeDelete(reinterpret_cast<node *>(ptr));}
+};
+using EbmlElementPtr = std::unique_ptr<ebml_element, NodeDeleter<ebml_element>>;
 
-typedef struct {
-	int64_t timecode_scale;
-	double duration;
-	char muxing_app[MAX_MKV_STRING_LENGTH];
-	char writing_app[MAX_MKV_STRING_LENGTH];
-} MKVSegmentInfo;
+struct StreamCloser {
+	void operator()(stream *ptr) {StreamClose(ptr);}
+};
+using StreamPtr = std::unique_ptr<stream, StreamCloser>;
 
-typedef struct {
-	uint8_t track_num;
-	uint32_t timestamp; // ms
-	bool_t keyframe;
-	uint8_t *data;
-	size_t data_length;
-	uint8_t *codec_state_data;
-	size_t codec_state_size;
-} MKVBlock;
+class MKVParserCtx {
+public:
+	MKVParserCtx();
+	~MKVParserCtx() noexcept;
+	MKVParserCtx(const MKVParserCtx &) = delete;
+	MKVParserCtx(MKVParserCtx &&) = delete;
+
+	parsercontext *get() noexcept;
+
+private:
+	void loadModules() noexcept;
+
+	parsercontext mParserCtx;
+};
+
+struct MKVSegmentInfo {
+	int64_t mTimecodeScale{0};
+	double mDuration{0.0};
+	std::string mMuxingApp{};
+	std::string mWritingApp{};
+
+	int parse(const ebml_element *seg_info_elt) noexcept;
+};
+
+struct MKVBlock {
+	uint8_t mTrackNum{0};
+	uint32_t mTimestamp{0}; // ms
+	bool mKeyframe{false};
+	std::vector<uint8_t> mData{};
+	std::vector<uint8_t>mCodecState{};
+};
 
 #define MKV_TRACK_TYPE_VIDEO    0x01
 #define MKV_TRACK_TYPE_AUDIO    0x02
@@ -51,125 +78,152 @@ typedef struct {
 #define MKV_TRACK_TYPE_BUTTONS  0x12
 #define MKV_TRACK_TYPE_CONTROL  0x20
 
-typedef struct {
-	uint8_t num;
-	uint64_t UID;
-	uint8_t type;
-	bool_t enabled;
-	bool_t def;
-	bool_t forced;
-	bool_t lacing;
-	int min_cache;
-	int max_block_addition_id;
-	char codec_id[MAX_MKV_STRING_LENGTH];
-	uint8_t *codec_private;
-	int codec_private_length;
-	int seek_preroll;
-} MKVTrack;
+struct MKVTrack {
+	uint8_t mNum{0};
+	uint64_t mUID{0};
+	uint8_t mType{0};
+	bool mEnabled{false};
+	bool mDef{false};
+	bool mForced{false};
+	bool mLacing{false};
+	int mMinCache{0};
+	int mMaxBlockAdditionId{0};
+	std::string mCodecId{};
+	std::vector<uint8_t> mCodecPrivate{};
+	int mSeekPreroll{0};
 
-typedef struct {
-	MKVTrack base;
-	bool_t interlaced;
-	int width;
-	int height;
-	double frame_rate;
-} MKVVideoTrack;
+	virtual ~MKVTrack() = default;
+	virtual void parse(const ebml_element *track_elt) noexcept;
 
-typedef struct {
-	MKVTrack base;
-	int sampling_freq;
-	uint8_t channels;
-} MKVAudioTrack;
+	static std::unique_ptr<MKVTrack> parseTrack(const ebml_element *track_elt) noexcept;
+};
 
-typedef struct _MKVTrackReader MKVTrackReader;
+struct MKVVideoTrack : public MKVTrack {
+	bool mInterlaced{false};
+	int mWidth{0};
+	int mHeight{0};
+	double mFrameRate{0.0};
 
+	void parse(const ebml_element *track_elt) noexcept override;
+};
 
-#ifdef __cplusplus
-extern "C" {
-#endif
+struct MKVAudioTrack : public MKVTrack {
+	int mSamplingFreq{0};
+	uint8_t mChannels{0};
 
-/**
- * @brief Open a MKV file for reading
- * @param filename Name of the file to open
- * @return A pointer on a MKVReader. NULL if opening fails
- */
-MKVReader *mkv_reader_open(const char *filename);
+	void parse(const ebml_element *track_elt) noexcept override;
+};
 
-/**
- * @brief Close a MKV file.
- * All associated track readers will be automatically destroyed
- * @param obj MKVReader
- */
-void mkv_reader_close(MKVReader *obj);
+class MKVTrackReader;
 
-/**
- * @brief Get information about the Matroska segment
- * @param reader MKVReader
- * @return Matroska segment information
- */
-const MKVSegmentInfo *mkv_reader_get_segment_info(const MKVReader *reader);
+class MKVReader{
+public:
+	MKVReader() = default;
+	MKVReader(const std::string &filename) {open(filename);}
+	MKVReader(const MKVReader &) = delete;
+	MKVReader(MKVReader &&) = delete;
+	~MKVReader() noexcept {close();}
 
-/**
- * @brief Get the default track for a specified track type
- * @param r MKVReader object
- * @param track_type Type of the track
- * @return A pointer on a track descriptor
- */
-const MKVTrack *mkv_reader_get_default_track(MKVReader *r, int track_type);
+	/**
+	 * @brief Open a MKV file for reading
+	 * @param filename Name of the file to open
+	 * @return A pointer on a MKVReader. NULL if opening fails
+	 */
+	void open(const std::string &filename);
+	/**
+	 * @brief Close a MKV file.
+	 * All associated track readers will be automatically destroyed
+	 * @param obj MKVReader
+	 */
+	void close() noexcept;
 
-/**
- * @brief Get the first track of the specified type
- * @param r MKVReader object
- * @param track_type Type of the track
- * @return A track descriptor
- */
-const MKVTrack *mkv_reader_get_first_track(MKVReader *r, int track_type);
+	/**
+     * @brief Get information about the Matroska segment
+     * @return Matroska segment information
+     */
+	const MKVSegmentInfo *getSegmentInfo() const noexcept {return mInfo.get();}
+	/**
+	 * @brief Get the default track for a specified track type
+	 * @param r MKVReader object
+	 * @param track_type Type of the track
+	 * @return A pointer on a track descriptor
+	 */
+	const MKVTrack *getDefaultTrack(int track_type) const noexcept;
+	/**
+	 * @brief Get the first track of the specified type
+	 * @param r MKVReader object
+	 * @param track_type Type of the track
+	 * @return A track descriptor
+	 */
+	const MKVTrack *getFirstTrack(int track_type) const noexcept;
 
-/**
- * @brief Create a track reader from its track number
- * @param reader MKVReader
- * @param track_num Track number
- * @return A pointer on a track reader
- */
-MKVTrackReader *mkv_reader_get_track_reader(MKVReader *reader, int track_num);
+	/**
+	 * @brief Create a track reader from its track number
+	 * @param reader MKVReader
+	 * @param track_num Track number
+	 * @return A pointer on a track reader
+	 */
+	MKVTrackReader *getTrackReader(int track_num) noexcept;
 
-/**
- * @brief Set the reading head of each assocated track reader at a specific position
- * @param reader MKVReader
- * @param pos_ms Position of the head in miliseconds
- * @return The effective position of the head after the operation
- */
-int mkv_reader_seek(MKVReader *reader, int pos_ms);
+	/**
+	 * @brief Set the reading head of each assocated track reader at a specific position
+	 * @param reader MKVReader
+	 * @param pos_ms Position of the head in miliseconds
+	 * @return The effective position of the head after the operation
+	 */
+	int seek(int pos_ms) noexcept;
 
-/**
- * @brief Get the next block
- * @param reader MKVReader
- * @param block Block data
- * @param end_of_track Return TRUE when the end of the track has been reached
- */
-void mkv_track_reader_next_block(MKVTrackReader *reader, MKVBlock **block, bool_t *end_of_track);
+private:
+	void resetPrivateData() noexcept;
+	int parseHeaders() noexcept;
+	int parseTracks(const ebml_element *tracks_elt) noexcept;
+	filepos_t findClusterPosition(int pos_ms) noexcept;
 
-/**
- * @brief Reset the track reader.
- * The reading head is set at the start of the track
- * @param reader MKVReader
- */
-void mkv_track_reader_reset(MKVTrackReader *reader);
+	std::unique_ptr<MKVParserCtx> mParserCtx{};
+	StreamPtr mFile{};
+	EbmlElementPtr mInfoElt{};
+	std::vector<EbmlElementPtr> mTracksElt{};
+	EbmlElementPtr mCues{};
+	std::unique_ptr<MKVSegmentInfo> mInfo{};
+	std::vector<std::unique_ptr<MKVTrack>> mTracks{};
+	filepos_t mFirstClusterPos{0};
+	filepos_t mLastClusterEnd{0};
+	filepos_t mFirstLevel1Pos{0};
+	std::list<std::unique_ptr<MKVTrackReader>> mReaders{};
 
-/**
- * @brief Destroy a track reader
- * @param reader Track reader to destroy
- */
-void mkv_track_reader_destroy(MKVTrackReader *reader);
+	friend class MKVTrackReader;
+};
 
-/**
- * @brief Free block data
- * @param block MKV block to free
- */
-void mkv_block_free(MKVBlock *block);
+class MKVTrackReader {
+public:
+	/**
+	 * @brief Get the next block
+	 * @param reader MKVReader
+	 * @param block Block data
+	 * @param end_of_track Return TRUE when the end of the track has been reached
+	 */
+	void nextBlock(std::unique_ptr<MKVBlock> &block, bool &end_of_track) noexcept;
+	/**
+	 * @brief Reset the track reader.
+	 * The reading head is set at the start of the track
+	 * @param reader MKVReader
+	 */
+	void reset() noexcept;
 
-#ifdef __cplusplus
-}
-#endif
+private:
+	void seek(filepos_t clusterPos) noexcept;
+	int seek(filepos_t clusterPos, int pos_ms) noexcept;
 
-#endif
+	static matroska_block *frameToBlock(const ebml_element *frameElt) noexcept;
+
+	int mTrackNum{0};
+	ebml_parser_context mParser{0};
+	const ebml_element *mTrackElt{nullptr};
+	EbmlElementPtr mCurrentCluster{};
+	const ebml_element *mCurrentFrameElt{nullptr};
+	StreamPtr mFile{};
+	MKVReader *mRoot{nullptr};
+	bool mNeedSeeking{false};
+
+	friend class MKVReader;
+};
