799c993ca9ca563bfe81610219b3318ea5977ee0
Julien Wadel <julien.wadel@belledonne-communications.com>
2021-02-09 22:30:21 +0100

    - on WIN32, build OpenGL related code with CXX flags - Fix unwanted function call for UWP - Reduce payload and MTU default for WSock2 - Fix potentially overflow on timer - Feature OpenGL : create a Window for X11 and UWP - Fix memory access when using mkv from dynamic library - Fix concurrency on MSMediaFoundation and rewrite some codes

--- src/videofilters/mkv.cpp
+++ src/videofilters/mkv.cpp
@@ -1645,7 +1645,7 @@ static matroska_block *matroska_write_block(Matroska *obj, const matroska_frame 
 			codecPrivateElt = (ebml_binary *)EBML_MasterAddElt(blockGroup, &MATROSKA_ContextCodecState, FALSE);
 			EBML_BinarySetData(codecPrivateElt, codecPrivateData, codecPrivateDataSize);
 		}
-		block->TrackNumber = trackNum;
+		MATROSKA_BlockSetTrackNum(block, trackNum);
 		MATROSKA_LinkBlockWithReadTracks(block, obj->tracks, TRUE);
 		MATROSKA_LinkBlockWriteSegmentInfo(block, obj->info);
 		MATROSKA_BlockSetKeyframe(block, isKeyFrame);
