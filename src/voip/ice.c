66748ac44f9e50c88ef42cb481e4dc46a0850c06
Simon Morlat <simon.morlat@belledonne-communications.com>
2021-03-03 09:41:49 +0100

    Attempt to fix an issue where TURN Sender Indications are lost, possibly because of invalid source address in mblk_t

--- src/voip/ice.c
+++ src/voip/ice.c
@@ -4179,6 +4179,7 @@ static bool_t ice_check_gathering_timeout(IceCheckList *cl, RtpSession *rtp_sess
 
 static void ice_send_stun_server_requests(IceStunServerRequest *request, IceCheckList *cl)
 {
+	
 	IceStunServerRequestTransaction *transaction = NULL;
 	MSTimeSpec curtime = ice_current_time();
 
