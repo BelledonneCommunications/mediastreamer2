371aef995de0b4d31a8cc8b205aab0256529eba5
Julien Wadel <julien.wadel@belledonne-communications.com>
2021-03-10 22:53:32 +0100

    Fix initialization build error

--- src/videofilters/generic_opengl_display.c
+++ src/videofilters/generic_opengl_display.c
@@ -85,7 +85,8 @@ struct _FilterData {
 };
 
 typedef struct _FilterData FilterData;
-static ms_mutex_t gLock = 0;	// Protect OpenGL call from threads
+static ms_mutex_t gLock;	// Protect OpenGL call from threads
+static bool_t gMutexInitialized = FALSE;
 
 // =============================================================================
 // Process.
@@ -120,7 +121,7 @@ static void ogl_init (MSFilter *f) {
 	data->mirroring = TRUE;
 	data->update_mirroring = FALSE;
 	data->prev_inm = NULL;
-	data->functions = {};
+	memset(&data->functions, 0, sizeof(data->functions));
 	data->functions.glInitialized = FALSE;
 	data->functions.eglInitialized = FALSE;
 	data->video_mode = MS_FILTER_VIDEO_AUTO;
@@ -130,8 +131,10 @@ static void ogl_init (MSFilter *f) {
 	ms_mutex_init(&data->lock, NULL);
 	data->rendering = FALSE;
 	f->data = data;
-	if(gLock == 0)
+	if(!gMutexInitialized){
+		gMutexInitialized = TRUE;
 		ms_mutex_init(&gLock, NULL);
+	}
 	ms_thread_create(&data->renderThread, NULL, threadRendering, f->data);// Create a rendering thread for the current filter
 }
 
