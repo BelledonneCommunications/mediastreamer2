fcbb5dc95dd308c02d27a396c2b80f94be3e3537
Julien Wadel <julien.wadel@belledonne-communications.com>
2021-03-19 00:39:01 +0100

    Fix MSFilterDesc for build error

--- src/videofilters/msqogl.cpp
+++ src/videofilters/msqogl.cpp
@@ -240,7 +240,7 @@ static MSFilterMethod methods[] = {
 	//{ MS_OGL_RENDER, qogl_call_render }, // qogl_call_render is autocalled by Qt, there is no need to put it in interface
 	{ 0, NULL }
 };
-#ifdef _WIN32
+
 MSFilterDesc ms_qogl_desc = {
 	MS_FILTER_PLUGIN_ID,
 	"MSQOGL",
@@ -257,20 +257,6 @@ MSFilterDesc ms_qogl_desc = {
 	methods
 };
 
-#else
-MSFilterDesc ms_qogl_desc = {
-	.id = MS_FILTER_PLUGIN_ID,
-	.name = "MSQOGL",
-	.text = "A Qt opengl video display",
-	.category = MS_FILTER_OTHER,
-	.ninputs = 2,
-	.noutputs = 0,
-	.init = qogl_init,
-	.process = qogl_process,
-	.uninit = qogl_uninit,
-	.methods = methods
-};
-#endif
 #ifndef VERSION
 #define VERSION "debug"
 #endif
