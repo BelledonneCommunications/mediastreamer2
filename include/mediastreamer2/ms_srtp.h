7eacdaca6537e7d06794842980c7be8a323ed6de
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2021-03-22 16:36:09 +0100

    Add method to know when using an unencrytped crypto suite

--- include/mediastreamer2/ms_srtp.h
+++ include/mediastreamer2/ms_srtp.h
@@ -55,6 +55,7 @@ typedef struct _MSCryptoSuiteNameParams{
 
 MS2_PUBLIC MSCryptoSuite ms_crypto_suite_build_from_name_params(const MSCryptoSuiteNameParams *nameparams);
 MS2_PUBLIC int ms_crypto_suite_to_name_params(MSCryptoSuite cs, MSCryptoSuiteNameParams *nameparams);
+MS2_PUBLIC bool_t ms_crypto_suite_is_unencrypted(MSCryptoSuite cs);
 
 
 /* defined in srtp.h*/
