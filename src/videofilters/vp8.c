3d79f641ff401824293ca4ec24eaffcdf6d084ff
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2021-05-18 14:28:45 +0200

    Protect fps change with mutexes and lock mutex before adding a task

--- src/videofilters/vp8.c
+++ src/videofilters/vp8.c
@@ -196,7 +196,6 @@ static void enc_init_impl(MSFilter *f){
 	s->cfg.g_error_resilient = VPX_ERROR_RESILIENT_DEFAULT|VPX_ERROR_RESILIENT_PARTITIONS;
 	s->cfg.g_lag_in_frames = 0;
 
-
 #if defined(__ANDROID__) || (TARGET_OS_IPHONE == 1) || defined(__arm__) || defined(_M_ARM)
 	cpuused = 10 - s->cfg.g_threads; /*cpu/quality tradeoff: positive values decrease CPU usage at the expense of quality*/
 	if (cpuused < 7) cpuused = 7; /*values beneath 7 consume too much CPU*/
@@ -639,7 +638,9 @@ static void enc_process(MSFilter *f) {
 	if ((entry_f = ms_queue_peek_last(f->inputs[0])) != NULL) {
 		ms_queue_remove(f->inputs[0], entry_f);
 		putq(&s->entry_q, entry_f);
+		ms_mutex_lock(&s->vp8_mutex);
 		ms_worker_thread_add_task(s->process_thread, enc_process_frame_task, (void*)f);
+		ms_mutex_unlock(&s->vp8_mutex);
 	}
 
 	/* Put each frame we have in exit_q in f->output[0] */
@@ -654,6 +655,7 @@ static void enc_process(MSFilter *f) {
 static void enc_postprocess(MSFilter *f) {
 	EncState *s = (EncState *)f->data;
 	ms_worker_thread_destroy(s->process_thread, FALSE);
+	s->process_thread = NULL;
 	if (s->ready) vpx_codec_destroy(&s->codec);
 	vp8rtpfmt_packer_uninit(&s->packer);
 	flushq(&s->entry_q,0);
