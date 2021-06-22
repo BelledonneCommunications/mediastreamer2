6014ecad1895c789f6b369491cda967858bbf869
Danmei Chen <danmei.chen@belledonne-communications.com>
2021-06-22 21:32:31 +0200

    fix build small sdk

--- src/voip/video-conference-all-to-all.cpp
+++ src/voip/video-conference-all-to-all.cpp
@@ -17,6 +17,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#ifdef VIDEO_ENABLED
 #include "video-conference.h"
 #include "mediastreamer2/msconference.h"
 
@@ -196,3 +197,4 @@ VideoConferenceAllToAll::~VideoConferenceAllToAll() {
 }
 
 }// namespace ms2
+#endif
--- src/voip/video-conference-one-to-all.cpp
+++ src/voip/video-conference-one-to-all.cpp
@@ -17,6 +17,7 @@
 * along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
+#ifdef VIDEO_ENABLED
 #include "video-conference.h"
 #include "mediastreamer2/msconference.h"
 
@@ -143,3 +144,4 @@ VideoConferenceOneToAll::~VideoConferenceOneToAll() {
 
 
 } // namespace ms2
+#endif
