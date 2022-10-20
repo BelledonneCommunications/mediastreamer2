2a993c65e56a7612565cc47c07c856bcb0b8197a
Andrea Gianarda <andrea.gianarda@belledonne-communications.com>
2022-10-20 16:58:39 +0200

    Change license to GNU Affero General Public License version 3

--- src/voip/offeranswer.c
+++ src/voip/offeranswer.c
@@ -1,21 +1,21 @@
 /*
- * offeranswer.c - Codec dependant implementation of offer/answer (rfc3264)
+ * Copyright (c) 2010-2022 Belledonne Communications SARL.
  *
- * Copyright (C) 2019 Belledonne Communications, Grenoble, France
+ * This file is part of mediastreamer2 
+ * (see https://gitlab.linphone.org/BC/public/mediastreamer2).
  *
- *  This program is free software; you can redistribute it and/or modify
- *  it under the terms of the GNU General Public License as published by
- *  the Free Software Foundation; either version 2 of the License, or
- *  (at your option) any later version.
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
  *
- *  This program is distributed in the hope that it will be useful,
- *  but WITHOUT ANY WARRANTY; without even the implied warranty of
- *  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- *  GNU General Public License for more details.
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
  *
- *  You should have received a copy of the GNU General Public License
- *  along with this program; if not, write to the Free Software
- *  Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA 02111-1307, USA.
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include "mediastreamer2/mscodecutils.h"
